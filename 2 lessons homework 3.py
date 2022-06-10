b6f49b15d29f763e5737b820c4dfcd736ee74cd7
DmitriyVavrinyuk <64530584+DmitriyVavrinyuk@users.noreply.github.com>
2022-06-10 13:38:17 +0300

    init commite

--- 2 lessons homework 3.py
+++ 2 lessons homework 3.py
@@ -9,19 +9,18 @@ http = urllib3.PoolManager()
 value = ["GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH", "HEAD", "TRACE"]
 url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
 
+# response_2 = requests.put(url, params={"method": i})
+# print(response_2.text)
+
 for i in value:
     for j in value:
-        if j == "GET":
-            params = {"method": j}
-        else:
-            params = {"method": j}
+        params = {"method": j}
         response = http.request(i, url, fields=params)
         # print(response.data.decode('utf-8'))
-        print(f"Type request is = {i}, status is = {response.status}, answer = {response.data.decode('utf-8')}, for method = {params}")
+        print(f"Type request is = {i}, status is = {response.status}, answer = {response.data}, for method = {params}")
     print("\n")
 
-
-
-
+    # response_2 = requests.put(url, data={"method": "POST"})
+    # print(response_2.text)
 
 
