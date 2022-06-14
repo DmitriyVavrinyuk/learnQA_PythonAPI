0facd6fbbda86a69c3b3994a466af4ffd6494a29
DmitriyVavrinyuk <64530584+DmitriyVavrinyuk@users.noreply.github.com>
2022-06-15 00:16:00 +0300

    init commite

--- 2 lessons homework 3.py
+++ 2 lessons homework 3.py
@@ -9,15 +9,13 @@ http = urllib3.PoolManager()
 value = ["GET", "POST", "DELETE", "PUT", "OPTIONS", "PATCH", "HEAD", "TRACE"]
 url = "https://playground.learnqa.ru/ajax/api/compare_query_type"
 
-# response_2 = requests.put(url, params={"method": i})
-# print(response_2.text)
-
 for i in value:
     for j in value:
-        params = {"method": j}
-        response = http.request(i, url, fields=params)
-        # print(response.data.decode('utf-8'))
-        print(f"Type request is = {i}, status is = {response.status}, answer = {response.data}, for method = {params}")
+
+            params = {"method": j}
+            response = http.request(i, url, fields=params)
+            # print(response.data.decode('utf-8'))
+            print(f"Type request is = {i}, status is = {response.status}, answer = {response.data}, for method = {params}")
     print("\n")
 
     # response_2 = requests.put(url, data={"method": "POST"})
---
+++ 2 lessons homework 5.py
@@ -0,0 +1,39 @@
+import requests
+import urllib3
+import time
+
+# Ex9*: Подбор пароля
+
+# http = urllib3.PoolManager()
+# method = "POST"
+url = "https://playground.learnqa.ru/ajax/api/get_secret_password_homework"
+auth_url = "https://playground.learnqa.ru/ajax/api/check_auth_cookie"
+login = "super_admin"
+exercise_password = {'888888', 'iloveyou', 'freedom', 'ninja', '12345', '123qwe', 'starwars', 'zaq1zaq1', 'hello', 'loveme',
+           'azerty', 'football', '7777777', 'admin', '1q2w3e4r', 'bailey', '654321', 'solo', '123456', 'ashley',
+           'welcome', 'photoshop[a]', '1qaz2wsx', 'qwerty123', '111111', 'passw0rd', 'lovely', 'aa123456', 'hottie',
+           'jesus', 'princess', 'michael', 'qwertyuiop', 'charlie', 'Football', 'abc123', '12345678', 'abc123 ',
+           'dragon', 'password1', '696969', 'donald', 'access', 'letmein', 'superman', 'monkey', 'trustno1', '555555', 'shadow',
+           'baseball', 'adobe123[a]', '1234', '000000', '1234567890', 'qazwsx', '!@#$%^&*', 'mustang', '121212',
+           'password', 'master', 'qwerty', '666666', 'flower', 'sunshine', 'whatever', 'login', '1234567', 'batman', '123456789',
+           '123123'}
+
+
+for i in exercise_password:
+    data = {"login": login, 'password': i}
+    response_get = requests.get(url, params=data)
+    # print(f"Please take your cooky = {dict(response_get.cookies)}")
+    auth_cookie = dict(response_get.cookies)
+
+    cookies = {}
+    if cookies is not None:
+        cookies.update(auth_cookie)
+        response_post = requests.post(auth_url, cookies=cookies)
+
+        if response_post.text == "You are authorized":
+            print(f"{response_post.text}.\nWelcome user '{login}', your password '{i}', your cookies is = '{auth_cookie['auth_cookie']}'")
+            break
+    else:
+        print(f"You are NOT authorized.\nSorry, your password '{i}', your cookies is = '{auth_cookie['auth_cookie']}' isn't True")
+print("The END")
+
